098eb695c3cf5f52330468969d564539fa97f1af
Chanakan1210 <chanakan121044@gmail.com>
2024-03-28 15:01:01 +0700

    .

---
+++ pages/Statistic.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie
+import requests
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+lottie_url_hello = "https://lottie.host/59528ad6-c294-40e3-bb93-d855204a6ffe/0VaAnvs7qX.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+st_lottie(lottie_hello,key="hello")
+
+
+
+st.page_link("home.py", label="หน้าแรก", icon="🏠")
+st.page_link("pages/Statistic.py", label="การนำเสนอข้อมูลด้วยสถถิติ", icon="1️⃣")
+st.page_link("pages/Chart.py", label="การนำเสนอข้อมูลด้วยการจินตทัศน์ข้อมูล", icon="2️⃣", disabled=False)
+st.page_link("http://www.google.com", label="Google", icon="🌎")
